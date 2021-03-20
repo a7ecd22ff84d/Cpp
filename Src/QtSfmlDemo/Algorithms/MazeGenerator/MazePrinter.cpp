045ee14f2366c0a5850e3f09918abc9edf481b29
Dawid Kozłowski <dawid_kozlowski@hotmail.com>
2021-03-20 14:00:32 +0100

    QtSfmlDemo: Created class for printing mazes

--- Src/QtSfmlDemo/Algorithms/MazeGenerator/MazePrinter.cpp
+++ Src/QtSfmlDemo/Algorithms/MazeGenerator/MazePrinter.cpp
@@ -1,16 +1,70 @@
 #include "QtSfmlDemo/Algorithms/MazeGenerator/MazePrinter.h"
 
 #include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 
-MazePrinter::MazePrinter(QtSfmlCanvas* canvas) : canvas(canvas), shape(20)
+const int wallThickness = 2;
+const int cellSize = 20;
+const int roomSize = cellSize - 2 * wallThickness;
+
+MazeCell::MazeCell(int row, int column)
 {
-	shape.setFillColor(sf::Color::Cyan);
-	shape.setPosition({100, 100});
+	room = sf::RectangleShape({roomSize, roomSize});
+	room.setPosition(
+		column * cellSize + wallThickness, row * cellSize + wallThickness);
+	room.setFillColor(sf::Color::Cyan);
+
+	eastPassage = sf::RectangleShape({wallThickness * 2, roomSize});
+	eastPassage.setPosition(
+		{room.getPosition().x + roomSize, room.getPosition().y});
+	eastPassage.setFillColor(sf::Color::Cyan);
+
+	southPassage = sf::RectangleShape({roomSize, wallThickness * 2});
+	southPassage.setPosition(
+		{room.getPosition().x, room.getPosition().y + roomSize});
+	southPassage.setFillColor(sf::Color::Cyan);
 }
 
-void MazePrinter:: draw()
+void MazeCell::draw(QtSfmlCanvas* canvas)
+{
+	canvas->draw(room);
+	canvas->draw(eastPassage);
+	canvas->draw(southPassage);
+}
+
+MazePrinter::MazePrinter(QtSfmlCanvas* canvas)
+	: canvas(canvas)
+	, border({600 - wallThickness * 2, 480 - wallThickness * 2})
+{
+	border.setPosition({wallThickness,wallThickness});
+	border.setOutlineThickness(wallThickness);
+	border.setOutlineColor(sf::Color::Red);
+	border.setFillColor(sf::Color::Transparent);
+
+	init();
+}
+
+void MazePrinter::init()
+{
+	for (int row = 0; row < 24; row++)
+	{
+		for (int column = 0; column < 30; column++)
+		{
+			mazeGrid[row][column] = MazeCell(row, column);
+		}
+	}
+}
+
+void MazePrinter::draw()
 {
 	canvas->clear();
-	canvas->draw(shape);
-	canvas->display();	
+	for (int row = 0; row < 24; row++)
+	{
+		for (int column = 0; column < 30; column++)
+		{
+			mazeGrid[row][column].draw(canvas);
+		}
+	}
+	canvas->draw(border);
+	canvas->display();
 }
--- Src/QtSfmlDemo/Algorithms/MazeGenerator/MazePrinter.h
+++ Src/QtSfmlDemo/Algorithms/MazeGenerator/MazePrinter.h
@@ -1,21 +1,39 @@
 #ifndef __MAZE_PRINTER_H
 #define __MAZE_PRINTER_H
 
-#include <SFML/Graphics/CircleShape.hpp>
-#include <qobjectdefs.h>
+#include <array>
+
+#include <SFML/Graphics/RectangleShape.hpp>
 
 #include "QtSfml/QtSfmlCanvas.h"
 
+class MazeCell
+{
+public:
+	MazeCell(){};
+	MazeCell(int x, int y);
+	void draw(QtSfmlCanvas* canvas);
+
+private:
+	sf::RectangleShape room;
+	sf::RectangleShape eastPassage;
+	sf::RectangleShape southPassage;
+};
+
 class MazePrinter
 {
+	using MazeGrid = std::array<std::array<MazeCell, 30>, 24>;
+
 public:
 	MazePrinter(QtSfmlCanvas* canvas);
 
+	void init();
 	void draw();
 
 private:
 	QtSfmlCanvas* canvas;
-	sf::CircleShape shape;
+	sf::RectangleShape border;
+	MazeGrid mazeGrid;
 };
 
 #endif // __MAZE_PRINTER_H
