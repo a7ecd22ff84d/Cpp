ea1b5b6dea3104b0e4aaca61dad60493977aa7ce
Dawid Kozłowski <dawid_kozlowski@hotmail.com>
2021-04-12 09:13:12 +0200

    QSD: Maze - Pseudo logging for qtsfml canvas

--- Src/QtSfml/QtSfmlCanvas.cpp
+++ Src/QtSfml/QtSfmlCanvas.cpp
@@ -1,10 +1,23 @@
 #include "QtSfml/QtSfmlCanvas.h"
 
+#include <iostream>
+#include <string_view>
+
 #include <QResizeEvent>
 #include <QWidget>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/View.hpp>
 #include <SFML/System/Vector2.hpp>
+#include <fmt/core.h>
+
+namespace
+{
+void log(std::string_view msg)
+{
+	// std::cout << msg;
+}
+
+} // namespace
 
 QtSfmlCanvas::QtSfmlCanvas(QWidget* parent) : QWidget(parent)
 {
@@ -36,6 +49,8 @@ void QtSfmlCanvas::resizeEvent(QResizeEvent* event)
 	auto height = static_cast<float>(event->size().height());
 	auto newSize = sf::Vector2f(width, height);
 
+	log(fmt::format("Widget resized to [{},{}]\n", width, height));
+
 	sf::RenderWindow::setSize(sf::Vector2u(width, height));
 
 	if (resizingPolicy == ResizingPolicy::keepZoomLevel)
@@ -48,6 +63,13 @@ void QtSfmlCanvas::resizeEvent(QResizeEvent* event)
 
 void QtSfmlCanvas::setViewArea(sf::Vector2f viewArea, sf::Vector2f center)
 {
+	log(fmt::format(
+		"View area set to [{},{}], center in [{},{}]\n",
+		viewArea.x,
+		viewArea.y,
+		center.x,
+		center.y));
+
 	this->viewArea = viewArea;
 	centerPoint = center;
 
@@ -58,5 +80,10 @@ void QtSfmlCanvas::setViewArea(sf::Vector2f viewArea, sf::Vector2f center)
 sf::Vector2f QtSfmlCanvas::calculateViewAreaKeepingAspectRatio(sf::Vector2f size)
 {
 	auto zoom = std::max(viewArea.x / size.x, viewArea.y / size.y);
-	return size * zoom;
+	auto visibleArea = size * zoom;
+
+	log(fmt::format(
+		"Calculated visible area [{},{}]\n", visibleArea.x, visibleArea.y));
+
+	return visibleArea;
 }
