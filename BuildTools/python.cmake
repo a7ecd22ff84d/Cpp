8b2cc4ac365080bd2ca4b59f2689f1a91811ece1
Dawid Kozłowski <dawid_kozlowski@hotmail.com>
2021-08-14 16:06:42 +0200

    CMake: Setting up virtual env before build
    From now on activated virtualenv is required to build project.

--- BuildTools/python.cmake
+++ BuildTools/python.cmake
@@ -1,18 +1,24 @@
-# find_package(Python3 REQUIRED COMPONENTS Interpreter)
+set(Venv_path ${CMAKE_SOURCE_DIR}/.venv)
+set(Requirements_file ${CMAKE_SOURCE_DIR}/requirements.txt)
+set(Cache_help "Used to track changes in requirements.txt file. Allows to install python packages only when necessary")
+set(Requirements_cached_timestamp 0 CACHE STRING ${Cache_help})
 
-set(VIRTUAL_ENV_PATH "${CMAKE_SOURCE_DIR}/.venv")
-set(VENV_TIMESTAMP_FILE "${CMAKE_SOURCE_DIR}/build/.venv_timestamp")
+find_package (Python3 REQUIRED COMPONENTS Interpreter)
 
-# if ()
+if (NOT EXISTS ${Venv_path}/bin/activate)
+    execute_process(COMMAND ${Python3_EXECUTABLE} -m venv ${Venv_path})
+    message(FATAL_ERROR 
+        "Python virtual environment not found, created new one in ${Venv_path}\n"
+        "Please activate it and rerun CMake")
+elseif("$ENV{VIRTUAL_ENV}" STREQUAL "")
+    message(FATAL_ERROR
+        "Python virtual environment found in ${Venv_path}\n"
+        "Please activate it and rerun CMake")
+endif()
 
-# execute_process (COMMAND "${Python3_EXECUTABLE}" -m venv "${VIRTUAL_ENV_PATH}")
+file(TIMESTAMP ${Requirements_file} Requirements_timestamp)
 
-# set (ENV{VIRTUAL_ENV} "${VIRTUAL_ENV_PATH}")
-# set (Python3_FIND_VIRTUALENV FIRST)
-
-# unset (Python3_EXECUTABLE)
-# find_package (Python3 REQUIRED COMPONENTS Interpreter Development)
-
-# execute_process (COMMAND "${Python3_EXECUTABLE}" -m venv "${VIRTUAL_ENV_PATH}")
-
-# set up virtual env
+if (${Requirements_cached_timestamp} STRLESS ${Requirements_timestamp})
+    execute_process(COMMAND ${Python3_EXECUTABLE} -m pip install -r ${Requirements_file})
+    set(Requirements_cached_timestamp ${Requirements_timestamp} CACHE STRING ${Cache_help} FORCE)
+endif()
